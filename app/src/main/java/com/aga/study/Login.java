61d8cac429d7ea4c20a6ae05de00885dc3fe39cb
Aga <agnieszka@gmail.com>
2018-07-19 21:01:17 +0200

    errors

--- app/src/main/java/com/aga/study/Login.java
+++ app/src/main/java/com/aga/study/Login.java
@@ -2,13 +2,22 @@ package com.aga.study;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    EditText loginNameInput = (EditText) findViewById(R.id.loginInput);
+    EditText passwordInput = (EditText) findViewById(R.id.passwordInput);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-       
+
+    }
+    public void login(View view)
+    {
+        String loginNameText = loginNameInput.getText().toString();
+        String passwordText = passwordInput.getText().toString();
     }
 }
